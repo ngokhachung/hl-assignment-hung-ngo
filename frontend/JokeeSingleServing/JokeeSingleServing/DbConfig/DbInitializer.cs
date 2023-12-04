a8f6bc52c0bd45980a4c2dffe19cfb7425ebcfc0
DESKTOP-PB6G172\nkh <ngokhachung@hotmail.com>
2023-12-04 23:27:53 +0700

    [Backend] implement layout and view

--- frontend/JokeeSingleServing/JokeeSingleServing/DbConfig/DbInitializer.cs
+++ frontend/JokeeSingleServing/JokeeSingleServing/DbConfig/DbInitializer.cs
@@ -15,9 +15,10 @@ namespace JokeeSingleServing.DbConfig
             }
 
             context.Jokes.AddRange(
-                new Joke { Text = "Why did the chicken cross the road?", Likes = 0, Dislikes = 0 },
-                new Joke { Text = "What do you get when you cross a snowman and a vampire?", Likes = 0, Dislikes = 0 }
-                // Add more jokes as needed
+               new Joke { Text = "A child asked his father, \"How were people born?\" So his father said, \"Adam and Eve made babies, then their babies became adults and made babies, and so on.\" The child then went to his mother, asked her the same question and she told him, \"We were monkeys then we evolved to become like we are now.The child ran back to his father and said, \"You lied to me!\" His father replied, \"No, your mom was talking about her side of the family.\"", Likes = 0, Dislikes = 0 },
+                new Joke { Text = "Teacher: \"Kids,what does the chicken give you?\" Student: \"Meat!\" Teacher: \"Very good! Now what does the pig give you?\" Student: \"Bacon!\"Teacher: \"Great! And what does the fat cow give you?\" Student: \"Homework!\"\r\n\r\n", Likes = 0, Dislikes = 0 },
+                 new Joke { Text = "The teacher asked Jimmy, \"Why is your cat at school today Jimmy?\" Jimmy replied crying, \"Because I heard my daddy tell my mommy, 'I am going to eat that pussy once Jimmy leaves for school today!'\"", Likes = 0, Dislikes = 0 },
+                  new Joke { Text = "A housewife, an accountant and a lawyer were asked \"How much is 2+2?\" The housewife replies: \"Four!\". The accountant says: \"I think it's either 3 or 4. Let me run those figures through my spreadsheet one more time.\" The lawyer pulls the drapes, dims the lights and asks in a hushed voice, \"How much do you want it to be?\"", Likes = 0, Dislikes = 0 }
             );
 
             context.SaveChanges();
