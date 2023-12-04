b5d796a7e8da2e8d243fa1a4e51abf7875375274
DESKTOP-PB6G172\nkh <ngokhachung@hotmail.com>
2023-12-04 23:28:12 +0700

    [Backend] implement controller

---
+++ frontend/JokeeSingleServing/JokeeSingleServing/Controllers/JokeController.cs
@@ -0,0 +1,70 @@
+﻿using JokeeSingleServing.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JokeeSingleServing.Controllers
+{
+    public class JokeController : Controller
+    {
+        private readonly JokeContext _context;
+
+        public JokeController(JokeContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var votedJokes = Request.Cookies["votedJokes"]?.Split(',').Select(int.Parse).ToList() ?? new List<int>();
+
+            var joke = _context.Jokes
+                .Where(j => !votedJokes.Contains(j.Id))
+                .FirstOrDefault();
+
+            if (joke == null)
+            {
+                return Content("That's all the jokes for today! Come back another day!");
+            }
+
+            return View(joke);
+        }
+
+        [HttpPost]
+        public IActionResult Vote(int id, string voteType)
+        {
+            var joke = _context.Jokes.Find(id);
+
+            if (joke == null)
+            {
+                return NotFound();
+            }
+
+            var votedJokes = Request.Cookies["votedJokes"]?.Split(',').Select(int.Parse).ToList() ?? new List<int>();
+
+            if (votedJokes.Contains(id))
+            {
+                return Content("You have already voted for this joke.");
+            }
+
+            if (voteType == "like")
+            {
+                joke.Likes++;
+            }
+            else if (voteType == "dislike")
+            {
+                joke.Dislikes++;
+            }
+            else
+            {
+                return BadRequest("Invalid vote type.");
+            }
+
+            _context.SaveChanges();
+
+            // Add the voted joke to the cookie
+            votedJokes.Add(id);
+            Response.Cookies.Append("votedJokes", string.Join(',', votedJokes));
+
+            return RedirectToAction("Index");
+        }
+    }
+}
